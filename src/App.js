9a9f0b73fb32f4dde4b6169f22d36bc8b3214758
Ankur Phani <ankur@enhanzed.com>
2022-11-16 20:24:20 +0530

    asd

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import ReactGA from "react-ga";
 function App() {
  useEffect(() => {
   ReactGA.initialize("UA-249696688-1");
+  ReactGA.pageview(window.location.pathname + window.location.search);
  }, []);
 
  return (
