cf0e835e45e3c557059764879ceb852392039155
Ankur Phani <ankur@enhanzed.com>
2022-11-16 20:03:14 +0530

    GA added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import { ThemeProvider } from '@emotion/react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './App.css';
-import Footer from './Components/Footer';
-import Box from './Components/library/Box';
-import theme from './Components/library/theme';
-import About from './Pages/about';
-import Home from './Pages/home';
+import { useEffect } from "react";
+import { ThemeProvider } from "@emotion/react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer";
+import Box from "./Components/library/Box";
+import theme from "./Components/library/theme";
+import About from "./Pages/about";
+import Home from "./Pages/home";
+import ReactGA from "react-ga";
 
 function App() {
-  return (
-    <>
-    <ThemeProvider theme={theme}>
-      <Box
-      position='relative'
-      minHeight='100%'>
-        <Router>
-        <Routes>
-          <Route exact path="/" element={<Home/>}/>
-          <Route exact path="/about" element={<About/>}/>
-        </Routes>
-      </Router>
-      <Footer/>
-      </Box>
+ useEffect(() => {
+  ReactGA.initialize("G-5J28M9SXR0");
+ }, []);
 
-
-    </ThemeProvider>
-
-    </>
-
-  );
+ return (
+  <>
+   <ThemeProvider theme={theme}>
+    <Box position="relative" minHeight="100%">
+     <Router>
+      <Routes>
+       <Route exact path="/" element={<Home />} />
+       <Route exact path="/about" element={<About />} />
+      </Routes>
+     </Router>
+     <Footer />
+    </Box>
+   </ThemeProvider>
+  </>
+ );
 }
 
 export default App;
